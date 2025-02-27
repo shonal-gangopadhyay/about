a167546e99a526fe7525d8e43433bcdb95392a64
Shonal Gangopadhyay <luffy.gangopadhyay@gmail.com>
2025-02-27 12:31:32 -0600

    add first blog update

--- posts/post1.js
+++ posts/post1.js
@@ -1,5 +1,20 @@
 window.post1 = {
-  title: "My First Blog Post",
+  title: "The website is live!",
   date: "2025-02-27",
-  content: "This is my first blog post! Excited to share more."
+  content: `This past Monday, February 24, I began building the HTML pages for my personal website, 
+  and today, February 27, I'm excited to share the final deployed version. 
+
+  While I had encountered HTML for the first time in 2004 when creating a Neopets page and later 
+  took formal courses in HTML (9th grade) and JavaScript (10th grade), I never expected that I 
+  would actually develop a full website of my own. 
+
+  The process of building this site has been both enjoyable and rewarding. I particularly 
+  appreciate the Request a 3D Print feature, as it integrates one of my hobbies into the 
+  project in a meaningful way. 
+
+  Looking ahead, I'm eager to continue adding new content, improving the website, and sharing 
+  my thoughts on a variety of topics, including technology, politics, my hobbies, and other 
+  ideas that come to mind. 
+
+  Thank you for visiting, and I look forward to sharing more soon!`
 };
